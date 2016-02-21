c140714bee61015893b5d95feb856a62a6bc6bff
Jonathan Slate <jslate@patientslikeme.com>
2016-02-20 21:40:29 -0500

    add cherry picking and rebasing commands

--- timmocs-commands.sh
+++ timmocs-commands.sh
@@ -74,5 +74,72 @@ git commit -m "add glasses"
 
 git merge curly
 
+git log --oneline --decorate
+
 git config --global core.mergeoptions --edit
 
+reset
+
+git branch hair
+git checkout -b horns
+echo 'left horn' >> face.txt
+git add *.txt
+git commit -m 'add one horn'
+echo 'add right horn' >> face.txt
+git add *.txt
+git commit -m 'add the other horn'
+
+git log --oneline --decorate
+
+
+git checkout hair
+echo 'hair' >> face_2.txt
+git add *.txt
+git commit -m 'add hair'
+echo 'beard' >> face_2.txt
+git add *.txt
+git commit -m 'add beard'
+
+git cherry-pick horns~1
+git cherry-pick horns~0
+
+git log --oneline --decorate
+
+
+
+
+
+reset
+
+git branch hair
+git checkout -b horns
+echo 'left horn' >> face.txt
+git add *.txt
+git commit -m 'add one horn'
+echo 'add right horn' >> face.txt
+git add *.txt
+git commit -m 'add the other horn'
+
+git log --oneline --decorate
+
+
+git checkout hair
+echo 'hair' >> face_2.txt
+git add *.txt
+git commit -m 'add hair'
+echo 'beard' >> face_2.txt
+git add *.txt
+git commit -m 'add beard'
+
+git rebase horns
+
+git log --oneline --decorate
+
+
+git checkout horns
+
+git merge hair
+
+git branch -d hair
+
+git log --oneline --decorate
